f7918b219687c4a5f56915676e0004ea39fa6476
Joonsuk Song <joonsuk0208@gmail.com>
2020-11-05 13:44:33 +0900

    final

--- gatormoisture.ts
+++ gatormoisture.ts
@@ -7,6 +7,7 @@
 * Written in Microsoft Makecode
 * Tested with a Dawon extension sensor and micro:bit
 *
+*
 * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
 * Please review the LICENSE.md file included with this example. If you have any questions
 * or concerns with licensing, please contact joonsuk0208@gmail.com.
